343ef4cd4f9cb79cf7ac8b83ba9a2cabeedd42ac
ApostolosMavro <apostolos.mavropoulos@allianceblock.io>
2023-10-26 14:48:07 +0300

    Move files to shared folder

--- constants.ts
+++
@@ -1,7 +0,0 @@
-export const maxFundsProvisionDuration = 60 * 60 * 24 * 7; // 1 week
-export const minInterestRate = 50000; //ethers.utils.parseUnits('0.05', 6); // 5%
-export const maxInterestRate = 150000; //ethers.utils.parseUnits('0.15', 6); // 15%
-export const maxPoolDuration = 60 * 60 * 24 * 360; // 1 year
-export const startingOrganizationFeePercentage = 20000; // 2 % // ethers.utils.parseUnits('0.02', 6)
-export const organizationFeePercentageOnInterest = 50000; // 5 % // ethers.utils.parseUnits('0.05', 6)
-export const stuckFundsDuration = 60 * 60 * 24 * 30 * 6; // 6 months
